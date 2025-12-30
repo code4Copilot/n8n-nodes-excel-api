0cac8dc29b4e88ad8184b5487007b1f236093014
Hueyan Chen <hueyan.chen@gmail.com>
2025-12-30 14:50:20 +0800

    chore: update gulp to version 5.0.1 in package.json

--- .eslintrc.prepublish.js
+++ .eslintrc.prepublish.js
@@ -12,5 +12,11 @@ module.exports = {
   },
   ignorePatterns: ['dist/', 'node_modules/'],
   extends: ['eslint:recommended'],
-  rules: {},
+  rules: {
+    // Allow unused vars if they are named 'this' (TypeScript type annotations)
+    'no-unused-vars': ['error', { 
+      varsIgnorePattern: '^this$',
+      argsIgnorePattern: '^this$'
+    }],
+  },
 };
